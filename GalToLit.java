ad250d719b10e57a27f19af553de37a619ddf8e8
madhav_18 <159808252+18-Madhav-9@users.noreply.github.com>
2024-07-12 17:11:04 +0530

    Add files via upload

---
+++ GalToLit.java
@@ -0,0 +1,16 @@
+/* 
+ * Convert Gallon To liter
+ * 1 Gallon = 3.7854
+*/
+
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gal ;
+        double lit ;
+        gal = 10 ;
+        lit = gal * 3.7854 ;
+        System.out.println(gal + " gallon is " + lit + " liters" ) ;
+
+    }
+}
