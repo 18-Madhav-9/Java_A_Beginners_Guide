04fb816f0d5324fe65726e42376d402e179d8ab5
madhav_18 <159808252+18-Madhav-9@users.noreply.github.com>
2025-02-24 21:21:11 +0530

    Add files via upload

---
+++ SelfTestThree_LOWtoUP.java
@@ -0,0 +1,30 @@
+
+/* 
+write a program
+that reads characters from the keyboard. Have it convert all lowercase letters to uppercase,
+and all uppercase letters to lowercase, displaying the result. Make no changes to any other
+character. Have the program stop when the user enters a period. At the end, have the program
+display the number of case changes that have taken place.
+
+*/
+
+public class SelfTestThree_LOWtoUP {
+    public static void main(String[] args ) throws java.io.IOException {
+        char c ; 
+        int count = 0 ;
+
+        do {
+            c = (char) System.in.read() ;
+            if ( c >= 97 && c <= 122 ) { // checks if character is Lowercase or not 
+                c = (char) (c - 32) ; // then decrease 32 to make it Uppercase
+                count++ ; // increase counter to denote case change
+            }
+            System.out.print(c ) ;
+
+        }while ( c != '.') ;
+
+        System.out.println();
+        System.out.println("Total changes :" + count) ;
+
+    }    
+}
