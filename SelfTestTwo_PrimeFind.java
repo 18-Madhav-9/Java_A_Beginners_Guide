a987349044351171ff78e3454adf9fc98ebb3137
madhav_18 <159808252+18-Madhav-9@users.noreply.github.com>
2025-02-24 17:29:14 +0530

    Add files via upload

--- SelfTestTwo_PrimeFind.java
+++ SelfTestTwo_PrimeFind.java
@@ -1,19 +1,19 @@
-
+// program that finds all of the prime numbers between 2 and 100.
 
 public class SelfTestTwo_PrimeFind {
     public static void main(String[] args ) {
 
         for ( int i = 2 ; i <= 100 ; i ++) {
             int count = 0 ; 
-            for ( int j = 1 ;  j <= i ; j++ ) {
-                if ( i%j == 0 && count <= 2 ) {
-                    count++ ;
+            for ( int j = 1 ;  j <= i ; j++ ) { // inner loop will run from 1 to number 
+                if ( i%j == 0 ) { // check if number is divided by numbers before it 
+                    count++ ; // increment count
                 }
-                else if (count >= 3) {
-                    break ;
+                else if (count > 3) {
+                    break ; // if number of divison counts > 2 break loop for unnnecessoy divisons
                 }
             }
-            if (count == 2 ) {
+            if (count == 2 ) { // if number is exactly divided 2 times by itself and 1 
                 System.out.println(i);
             }
         }
